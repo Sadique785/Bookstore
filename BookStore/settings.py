6bd40216831cf83c9feb00fefb5a2647e971082b
Ubuntu <ubuntu@ip-172-31-35-76.eu-north-1.compute.internal>
2024-03-07 06:52:55 +0000

    Host change

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-s5tofnaob#ct6e4_&mod_vu9551yq$xn0s547n8-74g#5@p2i-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
